3907eb20bd95877f8b9c9b5715b7225fb93471ae
Tejasgupta74 <157612264+Tejasgupta74@users.noreply.github.com>
2026-02-18 22:35:11 +0530

    Fix mailer

--- routes/auth.js
+++ routes/auth.js
@@ -84,7 +84,7 @@ function getMailer() {
     const nodemailer = require('nodemailer');
     return nodemailer.createTransport({
       host: process.env.SMTP_HOST,
-      port: process.env.SMTP_PORT ? parseInt(process.env.SMTP_PORT) : 587,
+      port: process.env.SMTP_PORT ? parseInt(process.env.SMTP_PORT) : 465,
       secure: process.env.SMTP_SECURE === 'true',
       auth: { user: process.env.SMTP_USER, pass: process.env.SMTP_PASS },
     });
