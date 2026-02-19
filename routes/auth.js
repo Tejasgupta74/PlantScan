eed437352b13a3c6412b045934d85c1423911ba6
Tejasgupta74 <157612264+Tejasgupta74@users.noreply.github.com>
2026-02-19 20:31:57 +0530

    u

--- routes/auth.js
+++ routes/auth.js
@@ -88,7 +88,7 @@ function getMailer() {
     return nodemailer.createTransport({
       host: process.env.SMTP_HOST,
       port: Number(process.env.SMTP_PORT),
-      secure: process.env.SMTP_SECURE === "true",
+      secure: process.env.SMTP_SECURE === "false",
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
